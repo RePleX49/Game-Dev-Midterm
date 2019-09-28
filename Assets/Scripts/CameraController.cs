ed527bc4b54cfe0e8700287e72aec0067e9291a6
RePleX49 <lukelin2009@hotmail.com>
2019-09-28 18:21:24 -0400

    Tweaked Camera and Added Dissolve Mat
    - Added Camera Lag
    -Added Dissolve Material

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,7 +7,7 @@ public class CameraController : MonoBehaviour
     // Target is the camera boom
     [SerializeField] Transform Target, Player;
     [SerializeField] float TurnSpeed = 1;
-    [SerializeField] float LerpTime = 0.5f;
+    [SerializeField] float CameraLagSpeed = 0.5f;
     float MouseX;
     float MouseY;
 
@@ -17,23 +17,21 @@ public class CameraController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
     }
 
+    private void FixedUpdate()
+    {
+        Vector3 TargetPosition = Player.position;
+        Vector3 SmoothedPosition = Vector3.Lerp(Target.position, TargetPosition, CameraLagSpeed * Time.deltaTime);
+        Target.position = SmoothedPosition;
+    }
+
     // Update is called once per frame
     void LateUpdate()
     {
         MouseX += Input.GetAxis("Mouse X") * TurnSpeed;
         MouseY -= Input.GetAxis("Mouse Y") * TurnSpeed;
-        MouseY = Mathf.Clamp(MouseY, -30, 60);
-
-        transform.LookAt(Target);
+        MouseY = Mathf.Clamp(MouseY, -30, 45);
 
         Target.rotation = Quaternion.Euler(MouseY, MouseX, 0);
         Player.rotation = Quaternion.Euler(0, MouseX, 0);
-    }
-
-    IEnumerator CameraLerp()
-    {
-        Quaternion NewRotation = Quaternion.Euler(MouseY, MouseX, 0);
-        Target.rotation = Quaternion.Lerp(Target.rotation, NewRotation, LerpTime);
-        yield return null;
     }
 }
--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    Rigidbody rb;
+    Vector3 NewPos;
+
+    float MoveHorizontal;
+    float MoveVertical;
+
+    public float MoveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveHorizontal = Input.GetAxis("Horizontal");
+        MoveHorizontal *= MoveSpeed * Time.deltaTime;
+
+        MoveVertical = Input.GetAxis("Vertical");
+        MoveVertical *= MoveSpeed * Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+        rb.position += (transform.forward * MoveVertical);
+        rb.position += (transform.right * MoveHorizontal);
     }
 }
