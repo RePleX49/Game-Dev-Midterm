82f56989319420164826d7c42b3d443d561b37da
Luke Lin <lukelin2009@hotmail.com>
2019-10-15 16:16:24 -0400

    Class mods

---
+++ Assets/Scripts/FloatingScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingScript : MonoBehaviour
+{
+    [SerializeField] float RotationMultiplier = 2.0f;
+
+    void Update()
+    {
+        transform.Rotate(Random.rotation.eulerAngles * Time.deltaTime * RotationMultiplier);
+    }
+}
